d4a78b76ad1d949b214c2f7c7ac9784a1a53c441
katsumasa <katsumasa@unity3d.com>
2023-01-17 15:36:09 +0900

    作り直し

---
+++ Editor/Scripts/AssetTreeView.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using System.IO;
+using UnityEngine.UI;
+using System.Security.Cryptography;
+using UTJ.UnityAssetBundleDumper.Editor;
+using System.Runtime.InteropServices;
+using UnityEditorInternal.Profiling.Memory.Experimental;
+using System;
+
+namespace UTJ.AssetBundleDumper.Editor
+{
+
+    public class AssetTreeViewItem : TreeViewItem
+    {
+        CABInfo m_cabInfo;
+        long m_pathID;
+
+        public CABInfo cabInfo
+        {
+            get { return m_cabInfo; }
+            set { m_cabInfo = value; }
+        }
+
+        public long pathID
+        {
+            get { return m_pathID; }
+            set { m_pathID = value; }
+        }
+
+    }
+
+
+    public class AssetTreeView : TreeView
+    {
+        public Action<int> doubleClickedItemCB;
+
+                
+        public AssetBundleDumpData assetBundleDumpData
+        {
+            set { m_assetBundleDumpData = value; }
+        }
+        public string cabName
+        {
+            get { return m_cabName; }
+            set { m_cabName = value; }
+        }
+
+        AssetBundleDumpData m_assetBundleDumpData;
+        string m_cabName;
+
+
+
+        public AssetTreeView(TreeViewState state) : base(state)
+        {
+            showBorder = false;
+            showAlternatingRowBackgrounds = true;
+        }
+
+        public TreeViewItem FindItem(int id)
+        {
+            return FindItem(id, rootItem);
+        }
+
+        public TreeViewItem FindItem(long pathID)
+        {
+            var items = GetRows();
+            foreach(var item in items)
+            {
+                var assetTreeViewItem = item as AssetTreeViewItem;
+                if(assetTreeViewItem == null)
+                {
+                    continue;
+                }
+                if(assetTreeViewItem.depth != 0)
+                {
+                    continue;
+                }
+                if(assetTreeViewItem.pathID == pathID)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        protected override void DoubleClickedItem(int id)
+        {
+            base.DoubleClickedItem(id);
+            if (doubleClickedItemCB != null)
+            {
+                doubleClickedItemCB(id);
+            }
+        }
+
+        protected override TreeViewItem BuildRoot()
+        {         
+            var cabInfo = m_assetBundleDumpData.GetCABInfo(m_cabName);
+            int depth = -1;
+            int id = 0;
+            TreeViewItem root = new TreeViewItem { id = id++, depth = depth, displayName = "Root" };
+            if (cabInfo == null)
+            {
+                var viewItem = new TreeViewItem { id = id++, depth = 0, displayName = "" };
+                root.AddChild(viewItem);
+            }
+            else if(cabInfo.assetInfos.Length == 0)
+            {
+                var viewItem = new TreeViewItem { id = id++, depth = 0, displayName = "" };
+                root.AddChild(viewItem);
+            }
+            else
+            {                
+                foreach(var assetInfo in cabInfo.assetInfos)
+                {
+                    string displayName = $"ID: {assetInfo.id} (ClassID: {assetInfo.classID}) {assetInfo.objectName} [Name: \"{assetInfo.name}\"]";
+
+                    var viewItem = new AssetTreeViewItem { id = id++, depth = 0, displayName = displayName };
+                    viewItem.pathID = assetInfo.id;
+                    viewItem.cabInfo = cabInfo;
+                    root.AddChild(viewItem);
+                    foreach(var pptrInfo in assetInfo.pptrInfos)
+                    {
+                        CABInfo externalCabInfo;
+
+                        // null‚Ìê‡ApathID‚ª0‚É‚È‚é
+                        if (pptrInfo.pathID == 0)
+                        {
+                            continue;
+                        }
+                        // “¯ˆêCAB“à‚Å‚ÌŽQÆ
+                        else if (pptrInfo.fileID == 0)
+                        {
+                            var refAssetInfo = cabInfo.GetAssetInfo(pptrInfo.pathID);
+                            displayName = $"ID: {refAssetInfo.id} (ClassID: {refAssetInfo.classID}) {refAssetInfo.objectName} [Name: \"{refAssetInfo.name}\"]";
+                            var refItem = new AssetTreeViewItem { id = id++, depth = 1, displayName = displayName };
+                            refItem.cabInfo = cabInfo;
+                            refItem.pathID = pptrInfo.pathID;
+                            viewItem.AddChild(refItem);
+                        }
+                        else
+                        {
+                            var cabName = cabInfo.externalReferences[pptrInfo.fileID - 1];
+                            externalCabInfo = m_assetBundleDumpData.GetCABInfo(cabName);
+
+                            if (externalCabInfo != null)
+                            {
+                                var externalAssetInfo = externalCabInfo.GetAssetInfo(pptrInfo.pathID);
+                                displayName = $"ID: {externalAssetInfo.id} (ClassID: {externalAssetInfo.classID}) {externalAssetInfo.objectName} [Name: \"{externalAssetInfo.name}\"] => {externalCabInfo.assetBundleName}/{cabName}";
+                                var externalItem = new AssetTreeViewItem { id = id++, depth = 1, displayName = displayName };
+                                externalItem.cabInfo = externalCabInfo;
+                                externalItem.pathID = pptrInfo.pathID;
+                                viewItem.AddChild(externalItem);
+                            }
+                            else
+                            {
+                                displayName = $"{cabName} ID: {pptrInfo.pathID}";
+                                var externalItem = new TreeViewItem { id = id++, depth = 1, displayName = displayName };
+                                viewItem.AddChild(externalItem);
+                            }
+                        }
+                    }
+                }
+            }
+            return root;
+        }
+    }
+}
---
+++ Editor/Scripts/ExternalReferenceTreeView.cs
@@ -0,0 +1,254 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using System.IO;
+using UnityEngine.UI;
+using System.Security.Cryptography;
+using UTJ.UnityAssetBundleDumper.Editor;
+using static Codice.Client.Common.Connection.AskCredentialsToUser;
+using System;
+
+namespace UTJ.AssetBundleDumper.Editor
+{
+    public class ExternalReferenceTreeViewItem : TreeViewItem
+    {
+        CABInfo m_cabInfo;
+
+        public CABInfo cabInfo
+        {
+            get { return m_cabInfo; }
+            set { m_cabInfo = value; }
+        }
+
+        public ExternalReferenceTreeViewItem() : base() { }
+    }
+
+
+
+    public class ExternalReferenceTreeView : TreeView
+    {
+#if UNITY_EDITOR_WIN
+        private const string k_RevealInFinderLabel = "Show in Explorer";
+#elif UNITY_EDITOR_OSX
+        private const string k_RevealInFinderLabel = "Reveal in Finder";
+#else
+        private const string k_RevealInFinderLabel = "Open Containing Folder";
+#endif
+
+        public Action selectionChangeCB;
+        public Action<int> doubleClickedItemCB;
+
+        public AssetBundleDumpData assetBundleDumpData
+        {
+            set { m_assetBundleDumpData = value; }
+        }
+
+        public string targetAssetBundlePath
+        {
+            set { m_targetAssetBundlePath = value; }
+        }
+        
+
+        AssetBundleDumpData m_assetBundleDumpData;
+        string m_targetAssetBundlePath;
+
+        ExternalReferenceTreeViewItem m_root;
+
+
+        public ExternalReferenceTreeView(TreeViewState state) : base(state)
+        {
+            showBorder = false;
+            showAlternatingRowBackgrounds = true;
+            
+        }
+
+        public TreeViewItem FindItem(int id)
+        {
+            return FindItem(id, rootItem);
+        }
+
+        public TreeViewItem FindItem(CABInfo cabInfo)
+        {
+            var items = GetRows();
+            foreach(var item in items)
+            {
+                var extItem = (ExternalReferenceTreeViewItem)item;
+                if(extItem.cabInfo == cabInfo)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        void RevealInFinder(object obj)
+        {
+            EditorUtility.RevealInFinder((string)obj);
+        }
+
+        protected override void ContextClickedItem(int id)
+        {
+            base.ContextClickedItem(id);
+            var menu = new GenericMenu();
+
+            var item = FindItem(id) as ExternalReferenceTreeViewItem;
+            if (item == null || item.cabInfo == null || item.cabInfo.path == string.Empty)
+            {
+                menu.AddDisabledItem(new GUIContent(k_RevealInFinderLabel));
+            }
+            else
+            {
+                menu.AddItem(new GUIContent(k_RevealInFinderLabel), false, RevealInFinder, item.cabInfo.path);
+            }
+
+            menu.ShowAsContext();
+        }
+
+        protected override void SelectionChanged(IList<int> selectedIds)
+        {
+            base.SelectionChanged(selectedIds);
+            if(selectionChangeCB != null)
+            {
+                selectionChangeCB();
+            }
+        }
+
+
+        protected override void DoubleClickedItem(int id)
+        {
+            base.DoubleClickedItem(id);
+            if(doubleClickedItemCB != null)
+            {
+                doubleClickedItemCB(id);
+            }
+        }
+
+        protected override TreeViewItem BuildRoot()
+        {
+            int depth = -1;
+            var root = new ExternalReferenceTreeViewItem { id = 0, depth = depth, displayName = "Root" };
+            var assetBundleName = Path.GetFileName(m_targetAssetBundlePath);
+            var assetBundleInfo =  m_assetBundleDumpData.GetAssetBundleInfo(assetBundleName);
+
+            m_root = root;
+
+            if (assetBundleInfo == null) 
+            {
+                var viewItem = new ExternalReferenceTreeViewItem { id = 1, depth = 1, displayName = "" };
+                root.AddChild(viewItem);
+                return root;
+            }            
+
+            var id = 1;
+            depth = 0;
+            // AssetBundle直下のCAB
+            foreach(var cabInfo in assetBundleInfo.cabInfos)
+            {
+                var treeViewItem = CreateTreeViewItem(cabInfo, id++,depth);
+                root.AddChild(treeViewItem);
+            }
+            // 各CAB内のExternal Referecnecsを再帰的に表示していく
+            for(depth = 0; depth < m_assetBundleDumpData.externalReferenceDepth; depth++)
+            {
+                if(CreateRows(ref id, depth) <= 0)
+                {
+                    break;
+                }
+            }
+
+            return root;
+        }
+
+        ExternalReferenceTreeViewItem CreateTreeViewItem(CABInfo cabInfo,int id,int depth)
+        {
+            var treeViewItem = new ExternalReferenceTreeViewItem { id = id,depth = depth};
+            treeViewItem.cabInfo = cabInfo;
+            if (cabInfo.assetBundleName == null || cabInfo.assetBundleName == string.Empty)
+            {
+                treeViewItem.displayName = $"{cabInfo.name}";
+            }
+            else
+            {
+                treeViewItem.displayName = $"{cabInfo.assetBundleName}/{cabInfo.name}";
+            }
+            return treeViewItem;
+        }
+
+        ExternalReferenceTreeViewItem CreateTreeViewItem(string dispName, int id, int depth)
+        {
+            var treeViewItem = new ExternalReferenceTreeViewItem { id = id, depth = depth };            
+            return treeViewItem;
+        }
+
+        // 指定された深さのItemに対して子のItemを作成する
+        int CreateRows(ref int id,int depth)
+        {
+            int count = 0;
+
+            // 指定されたdepthのItemのリストを作成
+            var externalReferenceTreeViewItems = new List<TreeViewItem>();
+            FindDepthItem(depth,ref externalReferenceTreeViewItems);
+           
+            foreach(var treeViewItem  in externalReferenceTreeViewItems)
+            {
+                ExternalReferenceTreeViewItem externalReferenceTreeViewItem = (ExternalReferenceTreeViewItem)treeViewItem;
+
+                foreach (var cabName in externalReferenceTreeViewItem.cabInfo.externalReferences)
+                {
+                    var cabInfo = m_assetBundleDumpData.GetCABInfo(cabName);
+                    if (cabInfo == null) 
+                    {
+                        var childItem = new ExternalReferenceTreeViewItem { id = ++id, depth = depth + 1,displayName = cabName };
+                        treeViewItem.AddChild(childItem);
+                    }
+                    else
+                    {
+                        var childItem = CreateTreeViewItem(cabInfo, ++id, depth + 1);
+                        treeViewItem.AddChild(childItem);
+                    }
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// 指定されたdepthのTreeViewItemの一覧を取得
+        /// </summary>
+        /// <param name="depth"></param>
+        /// <param name="treeViewItems"></param>
+        void FindDepthItem(int depth,ref List<TreeViewItem> treeViewItems)
+        {
+            FindDepthItemsRecursive(m_root, depth, ref treeViewItems);
+        }
+
+        /// <summary>
+        /// 指定されてdepthのTreeViewItemを再帰的に取得していく
+        /// </summary>
+        /// <param name="treeViewItem"></param>
+        /// <param name="depth"></param>
+        /// <param name="treeViewItems"></param>
+        void FindDepthItemsRecursive(TreeViewItem treeViewItem,int depth,ref List<TreeViewItem> treeViewItems)
+        {
+            if(treeViewItem.depth == depth)
+            {
+                treeViewItems.Add(treeViewItem);
+                return;
+            }
+            if(treeViewItem.depth > depth)
+            {
+                // ここは来ない筈
+                return;
+            }
+            if (treeViewItem.hasChildren)
+            {
+                foreach (var childItem in treeViewItem.children)
+                {
+                    FindDepthItemsRecursive(childItem, depth, ref treeViewItems);
+                }
+            }
+        }
+    }
+}
